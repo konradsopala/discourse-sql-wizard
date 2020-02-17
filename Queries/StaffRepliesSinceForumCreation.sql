6c0e915723c7fb5dcddf288b5a8b8a5f36e3c335
Conrad Sopala <konrad.sopala@gmail.com>
2020-02-17 14:50:37 +0100

    Adjusted syntax for StaffRepliesSinceForumCreation.sql

--- Queries/StaffRepliesSinceForumCreation.sql
+++ Queries/StaffRepliesSinceForumCreation.sql
@@ -1,17 +1,14 @@
 /* Number of  staff replies since forum creation - insert your date */
 
-SELECT p.user_id, count(p.id) as Replies
-    FROM posts p
-    WHERE p.post_type = 1 AND
-        p.post_number > 1 AND
-            (p.created_at >= date('2017-10-01') and
-            p.created_at < date(CURRENT_TIMESTAMP)) AND
-        p.user_id IN
-        (select u.id
-            from users u
-            where u.primary_group_id = 41) AND
---            where u.admin = 't' OR u.moderator = 't') AND
-        p.user_id not in ('-1', '-2') AND
-        p.topic_id not in ('4','24','26','32','33','34','35','36','37','38','39','40')
-GROUP BY p.user_id
+SELECT posts.user_id, count(posts.id) as replies
+    FROM posts
+    WHERE posts.post_type = 1 
+          AND posts.post_number > 1 
+          AND (posts.created_at >= date('2017-10-01') 
+          AND posts.created_at < date(CURRENT_TIMESTAMP)) 
+          AND posts.user_id IN
+            (SELECT u.id FROM users u WHERE u.primary_group_id = 41) 
+            AND posts.user_id NOT IN ('-1', '-2') 
+            AND posts.topic_id NOT IN ('4','24','26','32','33','34','35','36','37','38','39','40')
+GROUP BY posts.user_id
 ORDER BY Replies DESC
