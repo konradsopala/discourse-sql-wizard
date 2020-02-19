0c08df3420cb0d8467bd8eefbc288f76e02811ba
Conrad Sopala <konrad.sopala@gmail.com>
2020-02-19 18:48:12 +0100

    Update NewUsersByMonth.sql

--- Queries/NewUsersByMonth.sql
+++ Queries/NewUsersByMonth.sql
@@ -1,7 +1,7 @@
 /* Number of new users by month */
 
-SELECT concat(date_part('year', created_at), '-', date_part('month', created_at)) as Mth,
+SELECT concat(date_part('year', created_at), '-', date_part('month', created_at)) as month,
         count(id) AS count
 FROM "users"
-GROUP BY Mth
-ORDER BY Mth DESC
+GROUP BY month
+ORDER BY month DESC
