5a75ed6762fa3587a8b91c2c670b7164a34c460a
Conrad Sopala <konrad.sopala@gmail.com>
2020-02-19 18:55:51 +0100

    Update NumberOfNewTopicsPerCategoryInGivenMonth.sql

--- Queries/NumberOfNewTopicsPerCategoryInGivenMonth.sql
+++ Queries/NumberOfNewTopicsPerCategoryInGivenMonth.sql
@@ -3,10 +3,10 @@
 -- [params]
 -- int :months_ago = 1
 
-WITH query_period as (
+WITH timeframe as (
     SELECT
-        date_trunc('month', CURRENT_DATE) - INTERVAL ':months_ago months' AS period_start,
-        date_trunc('month', CURRENT_DATE) - INTERVAL ':months_ago months' + INTERVAL '1 month' - INTERVAL '1 second' AS period_end
+        date_trunc('month', CURRENT_DATE) - INTERVAL ':months_ago months' AS start,
+        date_trunc('month', CURRENT_DATE) - INTERVAL ':months_ago months' + INTERVAL '1 month' - INTERVAL '1 second' AS end
 )
 
 SELECT
@@ -14,8 +14,8 @@ SELECT
     count(1) AS topic_count
 FROM topics t
 RIGHT JOIN query_period qp
-    ON t.created_at >= qp.period_start
-        AND t.created_at <= qp.period_end
+    ON t.created_at >= timeframe.start
+        AND t.created_at <= timeframe.end
 WHERE t.user_id > 0
     AND t.category_id IS NOT NULL
 GROUP BY t.category_id
