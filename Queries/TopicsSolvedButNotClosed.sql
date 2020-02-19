435f17cb3e420b7c29dc6330953f0ccc29656fdb
Conrad Sopala <konrad.sopala@gmail.com>
2020-02-19 18:33:00 +0100

    Update TopicsSolvedButNotClosed.sql

--- Queries/TopicsSolvedButNotClosed.sql
+++ Queries/TopicsSolvedButNotClosed.sql
@@ -3,7 +3,5 @@
 SELECT 'https://community.auth0.com/t/' || id AS "TopicID", title
 FROM topics
 WHERE id IN (
-        SELECT target_topic_id
-        FROM user_actions
-        WHERE action_type = '15')
-    AND closed is false
+        SELECT target_topic_id FROM user_actions WHERE action_type = '15')
+        AND closed is false
