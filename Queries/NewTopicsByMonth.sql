97487cef4da0e1d122418e78aab14ee77cdd687c
Conrad Sopala <konrad.sopala@gmail.com>
2020-02-19 18:49:20 +0100

    Update NewTopicsByMonth.sql

--- Queries/NewTopicsByMonth.sql
+++ Queries/NewTopicsByMonth.sql
@@ -1,10 +1,10 @@
 /* Number of new topics by month */
 
-SELECT concat(date_part('year', created_at), '-', date_part('month', created_at)) AS Mth,
+SELECT concat(date_part('year', created_at), '-', date_part('month', created_at)) AS month,
     count(id) AS "New_Topics"
 FROM topics
 WHERE archetype <> 'private_message'
     AND user_id NOT IN ('-1', '-2')
     AND date_part('year', created_at) > '2017'
-GROUP BY Mth
-ORDER BY Mth DESC
+GROUP BY month  
+ORDER BY month DESC
