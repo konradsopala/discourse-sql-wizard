badcfbe058b42a806aa85807d06149a228f34830
Conrad Sopala <konrad.sopala@gmail.com>
2020-02-19 18:31:13 +0100

    Update UsersActiveLastMonth.sql

--- Queries/UsersActiveLastMonth.sql
+++ Queries/UsersActiveLastMonth.sql
@@ -4,10 +4,10 @@
 -- string :interval = 1 month
 -- string :trunc = month
 
-WITH t AS (
+WITH timeframe AS (
   SELECT date_trunc(:trunc, CURRENT_TIMESTAMP - INTERVAL :interval) AS start,
-    date_trunc(:trunc, CURRENT_TIMESTAMP) AS END
+    date_trunc(:trunc, CURRENT_TIMESTAMP) AS end
 )
 SELECT count(id) AS Monthly_Active
-FROM users, t
-WHERE last_seen_at BETWEEN t.start AND t.end
+FROM users, timeframe
+WHERE last_seen_at BETWEEN timeframe.start AND timeframe.end
