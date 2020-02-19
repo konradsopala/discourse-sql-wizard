42386a38349894d195515e0bc711e3443a3003a8
Conrad Sopala <konrad.sopala@gmail.com>
2020-02-19 18:32:03 +0100

    Update TopicsSolvedByMonth.sql

--- Queries/TopicsSolvedByMonth.sql
+++ Queries/TopicsSolvedByMonth.sql
@@ -2,10 +2,10 @@
 
 SELECT concat(
         date_part('year', created_at), '-',
-        date_part('month', created_at)) AS Month,
+        date_part('month', created_at)) AS month,
     count(id) AS total_solved
 FROM user_actions
 WHERE action_type = '15'
     AND date_part('year', created_at) > '2017'
-GROUP BY Month
-ORDER BY Month
+GROUP BY month
+ORDER BY month
